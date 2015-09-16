189c743ad340601e6577e673c79656d9bad008dc
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-09-16 19:01:59 +0200

    Use of an external manager to persist the OSM line data
    The line detail POST method, now persists the retrieved OSM data.
    
    To do that, it instantiates the persistence manager defined in the
    settings file, so different managers can be used. Anyway, the manager
    must follow two simple rules: it has to implement a constructor the line
    is passed to, and it has to implement a save() method to persist the
    data.

--- osmlineadapters/views.py
+++ osmlineadapters/views.py
@@ -6,6 +6,8 @@ from django.core.cache.backends import locmem
 
 import json
 
+import osmlineadapters.settings as settings
+
 @csrf_exempt
 def newline(request, company_id):
     if request.method == 'GET':
@@ -46,8 +48,19 @@ def newlinedetail(request, company_id, osm_line_id, manager=None):
         agree = bool(json_request['osmline']['accept'])
         
         if agree:
-            print("Storing line")
-        else:
-            cache_simplified_id = str(osm_line_id) + "_simple"
-            cache.delete(osm_line_id)
-            cache.delete(cache_simplified_id)
+            line = json.loads(cache.get(cache_simplified_id))
+            osmlinemanager = _get_osmlinemanager(line)
+            osmlinemanager.save()
+            
+        cache_simplified_id = str(osm_line_id) + "_simple"
+        cache.delete(osm_line_id)
+        cache.delete(cache_simplified_id)
+
+def _get_osmlinemanager(line=None):
+    manager_module = settings.OSMLINEMANAGERMODULE
+    manager_class_name = settings.OSMLINEMANAGERCLASS
+    mod = __import__(manager_module, fromlist=[manager_class_name])
+    class_ = getattr(mod, manager_class_name)
+    
+    instance = class_(line)
+    return instance
