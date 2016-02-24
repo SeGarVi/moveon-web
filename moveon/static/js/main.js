7badd9b72da369c28dfa8c9a70a5deecc4587063
Jiuck <jiuck.grecords@gmail.com>
2016-02-24 18:25:57 +0100

    Add part of the verification process in the timetable

--- moveon/static/js/main.js
+++ moveon/static/js/main.js
@@ -71,9 +71,7 @@ var timetableColumn_counter = 1;
 function add_timetableColumn(serialize_ids) {
     ///$( " form ").append('<div id="timetable-column-' + timetableColumn_counter + '" class="moveon-company timetable--form"></div>');
     $( " #timetable--form").append('<fieldset id="timetable-column-' + timetableColumn_counter + '"  class="moveon-company"></fieldset>');
-    console.log(serialize_ids.length);
     for (var i = 0; i < serialize_ids.length; i++) {
-        console.log(serialize_ids[i]);
         $( "#timetable-column-" + timetableColumn_counter).append('<input class="input" name="time-' + timetableColumn_counter + '-' + serialize_ids[i] + '" type="text"/><br>');
     }
     timetableColumn_counter++;
@@ -81,8 +79,9 @@ function add_timetableColumn(serialize_ids) {
 
 function send_timetableCalculation(route_id, stretch_id) {
     var mean_speed = $( "input[name='mean_speed']" ).val();
+
+    /*Only for non empty values*/
     var timetable_form_empties = $('form').serializeArray();
-    
     for (i=0; i<timetable_form_empties.length;) {
         var obj = timetable_form_empties[i]
         if(obj.value == "") {
@@ -112,28 +111,87 @@ function send_timetableCalculation(route_id, stretch_id) {
     });
 }
 
+function verify_last(var1, selector, alert_text){
+    if (var1==""){
+        $( selector ).addClass("redborder");
+        alert(alert_text);
+        return false;
+    }else{
+        $( selector ).removeClass("redborder");
+        return true;
+    }
+}
+
 function send_timetableAcceptation(route_id, stretch_id) {
-    var mean_speed = $( "input[name='mean_speed']" ).val();
+    var days = $("input[name=day]:checked").map(function () {return this.value;}).get().join(",");
     var start = $( "input[name='start-date']" ).val();
     var end = $( "input[name='end-date']" ).val();
-    var days = $("input[name=day]:checked").map(function () {return this.value;}).get().join(",");
-    var timetable_form_empties = $('form').serialize();
-    var timetable_form = timetable_form_empties.replace(/[^&]+=\.?(?:&|$)/g, '')
-    var timetable = {
-        "mean_speed":mean_speed,
-        "times": timetable_form,
-        "route_id": route_id,
-        "day": days,
-        "start": start,
-        "end": end
-    };
-    console.log(timetable + " - " + start + " - " + end + " - " + days);
+    var send = true;
 
-    $.ajax({
-      type: "PUT",
-      url: "/moveon/stretches/"+stretch_id,
-      data: JSON.stringify(timetable)//,
-      //success: console.log("success"),
-      //dataType: json
-    });
+    send = verify_last(days, '.moveon-company_day', 'Please, select at least one day in the week checkbox.');
+    send = verify_last(start, 'input[name="start"]', 'Please, select the start date for the timetable');
+    send = verify_last(end, 'input[name="end"]', 'Please, select the end date for the timetable');
+
+    if (send) {
+        var mean_speed = $( "input[name='mean_speed']" ).val();
+        /*Only for non empty values*/
+        var timetable_form_empties = $('form').serializeArray();
+        for (i=0; i<timetable_form_empties.length;) {
+            var obj = timetable_form_empties[i]
+            if(obj.value == "") {
+                timetable_form_empties.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+
+        var timetable = {
+            "mean_speed":mean_speed,
+            "times": timetable_form,
+            "route_id": route_id,
+            "day": days,
+            "start": start,
+            "end": end
+        };
+
+        $.ajax({
+          type: "PUT",
+          url: "/moveon/stretches/"+stretch_id,
+          data: JSON.stringify(timetable)//,
+          //success: console.log("success"),
+          //dataType: json
+        });
+    }
+}
+
+var verification = [];
+function verify(class_name, res) {
+    if (res==null) {
+        $( "input[name='"+class_name+"']" ).addClass("redborder");
+        verification.push(class_name);
+        $( "button" ).prop('disabled', true);
+    }else{
+        $( "input[name='"+class_name+"']" ).removeClass("redborder");
+        verification.splice(verification.indexOf(class_name), 1);
+        if (verification.length==0) {
+            $( "button" ).prop('disabled', false);
+        }
+    }
+}
+function verify_time(class_name) {
+    var input = $( "input[name='"+class_name+"']" ).val();
+    var res = input.match(/^([01]?[0-9]|2[0-3]):[0-5][0-9]/);
+    verify(class_name, res);
+}
+
+function verify_speed(class_name) {
+    var input = $( "input[name='"+class_name+"']" ).val();
+    var res = input.match(/^[01]?[0-9]?[0-9]\.?[0-9]?[0-9]?/);
+    verify(class_name, res);
+}
+
+function verify_date(class_name) {
+    var input = $( "input[name='"+class_name+"']" ).val();
+    var res = input.match(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}/);
+    verify(class_name, res);
 }
